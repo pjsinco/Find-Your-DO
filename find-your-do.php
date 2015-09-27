a11a6596ef4fa27de464cdb8bc0fe4385b2fa9ec
pjsinco <psinco@fas.harvard.edu>
2015-09-27 12:28:36 -0500

    Housekeeping

--- find-your-do.php
+++ find-your-do.php
@@ -1,18 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://github.com/pjsinco
- * @since             1.0.0
- * @package           Find_Your_Do
- *
- * @wordpress-plugin
  * Plugin Name:       Find Your DO
  * Plugin URI:        github
  * Description:       Find Your DO app
