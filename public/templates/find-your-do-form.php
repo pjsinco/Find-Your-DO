3452a84b47ada3277581326361544f5d3e877e74
pjsinco <psinco@fas.harvard.edu>
2015-09-29 11:00:57 -0500

    Add search form to top of results page

---
+++ public/templates/find-your-do-form.php
@@ -0,0 +1,21 @@
+<div class="row fyd">
+    <div class="col-md-3 hero--title">FIND YOUR DO</div>
+    <div class="col-md-9 hero--form">
+        <form id="findYourDo" action="">
+            <div class="hero--form--inner">
+                <div class="row">
+                    <div class="col-sm-6 location">
+                        <input id="specialty" placeholder="Name or specialty" name="q" type="text">
+                    </div>
+                    <div class="col-sm-6 speciality">
+                        <input id="location" placeholder="City, state or zip" name="location" type="text">
+                    </div>
+                </div> <!-- .row -->
+            </div><!-- .hero-form-inner -->
+            <span class="hero--form--button">
+                <button>GO <i class="fa fa-angle-double-right"></i></button>
+            </span>
+        </form>
+    </div> <!-- .col-md-9.hero-form -->
+</div> <!-- .row.fyd -->
+
--- public/templates/page-find-your-do-results.php
+++ public/templates/page-find-your-do-results.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<?php //get_template_part( 'modules/content', 'interiorHero' ); ?>
-
 <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : the_post(); ?>
 
@@ -9,29 +7,15 @@
 
             <div class="container-fluid">
 
-                <?php
-                    $outputLeftPageData = wp_nav_menu( array(
-                        'theme_location' => 'primary-menu', // location of menu
-                        'container_id'   => 'leftBar',
-                        'sub_menu'       => true, // activate custom sub_menu function
-                        'echo'           => false, // return menu as variable
-                        'menu_id'        => 'subMenu',
-                    ) );
-                ?>
+                <?php include plugin_dir_path(__FILE__) . 'find-your-do-form.php'; ?>
 
                 <div class="hasRightCol interior-content">
 
-                    <?php
-                        // LEFT
-                        echo $outputLeftPageData;
-                    ?>
-
                     <div id="right">
+
                         <div class="row">
 
                             <div id="content" class="mainContent">
-
-                                <?php echo 'hiya'; //pageTitle(); ?>
 
                                 <div class="entry facetwp-template">
                                 <?php the_content(); ?>
@@ -45,6 +29,7 @@
                             ?>
 
                         </div> <!-- .row -->
+
                     </div> <!-- #right  -->
 
                 </div> <!-- hasRightCol.interior-content -->
